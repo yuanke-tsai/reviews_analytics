8ac080bbd4f711d8b56f9cd1933c4d35591007b3
Yuanke Tsai <yuanke.tsai@gmail.com>
2021-03-24 13:51:17 +0800

    2

--- reviews_a.py
+++ reviews_a.py
@@ -7,12 +7,16 @@ Created on Wed Mar 24 10:14:34 2021
 """
 
 data = []
-count =0
 
-with open('reviews.txt', 'r') as f:
-    for line in f:
+with open('reviews.txt', 'r') as file:
+    for line in file:
         data.append(line)
-        #print(len(data))#，會印出清單1000000筆。運用％（餘數），使每1000筆再print！
-        count += 1
-        if count % 100000 == 0:
-            print(len(data))        
+
+base = []
+total_len = 0
+for base in data:
+    total_len = total_len + len(base)
+
+print('總留言字數：', total_len)
+print('留言總數：', len(data))
+print('平均留言個數：', total_len / len(data))
