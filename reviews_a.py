6c1ab8224ebb3133362c301319f6c57ef760cb3e
Yuanke Tsai <yuanke.tsai@gmail.com>
2021-03-26 09:55:45 +0800

    3

--- reviews_a.py
+++ reviews_a.py
@@ -5,6 +5,9 @@ Created on Wed Mar 24 10:14:34 2021
 
 @author: yuanke.tsai
 """
+#新增篩選功能
+#字數篩選
+#特徵篩選
 
 data = []
 
@@ -17,6 +20,21 @@ total_len = 0
 for base in data:
     total_len = total_len + len(base)
 
+#字數篩選        
+new = []
+for base in data:
+    if len(base) < 100:
+        new.append(base)
+
+#特徵篩選
+good = []
+for base in data:
+    if 'good' in base:
+        good.append(base)
+
+
 print('總留言字數：', total_len)
 print('留言總數：', len(data))
 print('平均留言個數：', total_len / len(data))
+print('留言字數小於一百字：', len(new), '筆')
+print('留言中包含good之數量：', len(good), '筆')
