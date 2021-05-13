893d5ea5d450fa6061a1b9d74c1a9c640ae21abc
Yuanke Tsai <yuanke.tsai@gmail.com>
2021-05-13 11:28:22 +0800

    add timing

--- reviews_a.py
+++ reviews_a.py
@@ -9,6 +9,8 @@ Created on Wed Mar 24 10:14:34 2021
 #字數篩選
 #特徵篩選>>清單快寫法
 #新增字典查詢
+#新增計時
+import time
 
 data = []
 
@@ -38,6 +40,8 @@ print('留言字數小於一百字：', len(new), '筆')
 print('留言中包含good之數量：', len(good), '筆')
 
 #文字查詢
+#計時
+start_time = time.time()
 wc = {} # word count
 for d in data:
     words = d.split()
@@ -50,8 +54,10 @@ for d in data:
 for word in wc:
     if wc[word] >= 1000000:
         print(word, wc[word])
+end_time = time.time()
 
-print(len(wc))
+print('run 留言花了：', end_time - start_time, '秒')
+print('字典長度：', len(wc))
 
 while True:
     user_input = input('請問想查詢什麼字？')
